b4a1907dd9e9b2d6212f37f1b160baa5f8fcd6a8
aashish0770 <60351454+aashish0770@users.noreply.github.com>
2023-10-15 22:45:30 +0545

    added pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
 
 export default function App() {
-  return (
-    <div>App</div>
-  )
+  return <BrowserRouter>
+  <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/about' element={<About/>}/>
+    <Route path='/profiles' element={<Profiles/>}/>
+    <Route path='/sing-in' element={<SingIn/>}/>
+    <Route path='/sing-up' element={<SingUp/>}/>
+  </Routes>
+  </BrowserRouter>;
 }
