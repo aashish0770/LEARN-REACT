d62bbc19aa41196324c5776d0da6576dc98a4323
aashish0770 <60351454+aashish0770@users.noreply.github.com>
2023-10-28 19:28:50 +0545

    update the header and profile is privte

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Profiles from './pages/Profiles'
 import SignIn from './pages/SignIn' 
 import SignUp from './pages/SignUp'
 import Header from './components/Header'
+import PrivateRoute from './components/PrivateRoute'
 
 
 export default function App() {
@@ -14,7 +15,9 @@ export default function App() {
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/about' element={<About/>}/>
-      <Route path='/profiles' element={<Profiles/>}/>
+      <Route element={< PrivateRoute />}>
+        <Route path='/profiles' element={<Profiles/>}/>
+      </Route>
       <Route path='/sign-in' element={<SignIn/>}/>
       <Route path='/sign-up' element={<SignUp/>}/>
     </Routes>
--- client/src/pages/Profiles.jsx
+++ client/src/pages/Profiles.jsx
@@ -5,3 +5,4 @@ export default function Profiles() {
     <div>Profiles</div>
   )
 }
+
